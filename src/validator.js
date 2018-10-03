bd5f82b152d1c0de35b7f6a19705ffbc23c5e7db
larshp <larshp@hotmail.com>
2018-10-03 06:37:41 +0200

    fix?

--- src/validator.js
+++ src/validator.js
@@ -162,7 +162,7 @@ function parse(result) {
 
     for (let file of files) {
       const buf = fs.readFileSync(file.path + file.name, "utf8");
-      afiles.push(new abaplint.File(file.name, buf));
+      afiles.push(new abaplint.MemoryFile(file.name, buf));
     }
 
     let config = abaplint.Config.getDefault();
