941df9e129dc029774fd313c15b7fc676e22c0e1
Lars Hvam <larshp@hotmail.com>
2018-10-03 06:54:18 +0200

    Update validator.js

--- src/validator.js
+++ src/validator.js
@@ -162,6 +162,7 @@ function parse(result) {
 
     for (let file of files) {
       const buf = fs.readFileSync(file.path + file.name, "utf8");
+      console.dir(abaplint);
       afiles.push(new abaplint.File(file.name, buf));
     }
 
