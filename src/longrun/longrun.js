fe8dc2e489ef8cd661bf1a06d709a7afd34aae75
larshp <larshp@hotmail.com>
2018-12-21 10:31:34 +0100

    statement stats

--- src/longrun/longrun.js
+++ src/longrun/longrun.js
@@ -19,12 +19,57 @@ function repoStats(folder, generated) {
   }
 }
 
+function statementStats(folder, generated) {
+  const files = fs.readdirSync(folder + "stats");
+  let result = [];
+  let total = [];
+  for (const filename of files) {
+    const stats = JSON.parse(fs.readFileSync(folder + "stats/" + filename, "utf8"));
+    total = total.concat(stats.statementTypes);
+  }
+
+  // todo, dont do it like this
+  for (const i of total) {
+    let found = false;
+    for (const r of result) {
+      if (r.type === i.type) {
+        found = true;
+        break;
+      }
+    }
+    if (found === false) {
+      result.push({type: i.type, count: 0});
+    }
+  }
+
+  for (const r of result) {
+    for (const i of total) {
+      if (r.type === i.type) {
+        r.count = r.count + i.count;
+      }
+    }
+  }
+
+  result.sort((a, b) => {
+    if (a.count > b.count) {
+      return -1;
+    } else if (a.count < b.count) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  fs.writeFileSync(folder + "statement_types.json", JSON.stringify(result, null, 2));
+}
+
 function longrun(folder) {
   console.log("Folder: " + folder);
   fsextra.ensureDirSync(workdir);
   const generated = JSON.parse(fs.readFileSync(folder + "generated.json", "utf8"));
 
   repoStats(folder, generated);
+  statementStats(folder, generated);
 }
 
 
